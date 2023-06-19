d3d8fea8d53ed3aa653f95fd82008b02e57c77bf
Zgibbs58 <zacharywgibbs@gmail.com>
2023-06-19 13:04:10 -0500

    Added generateSVG.js and  questions.js. Added a class called shape to the shapes.js file and connected these modules to index.js.

--- index.js
+++ index.js
@@ -1,55 +1,12 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+const generateSVG = require("./lib/generateSVG");
+const shape = require("./lib/shape");
 
 const prompt = inquirer.createPromptModule();
 
-prompt([
-  {
-    type: "input",
-    message: "Enter the three letter abbreviation for your logo:",
-    name: "text",
-    validate: function (answer) {
-      if (answer.length < 3 || answer.length > 3) {
-        return console.log(" Logo text must be 3 characters long.");
-      }
-      return true;
-    },
-  },
-  {
-    type: "list",
-    message: "Would you rather choose color by name or hex code?",
-    name: "colorChoice",
-    choices: ["name", "hex"],
-  },
-  {
-    type: "input",
-    message: "Enter the name of your text color:",
-    name: "colorName",
-    when: (answers) => answers.colorChoice.includes("name"),
-  },
-  {
-    type: "input",
-    message: "Enter the 6 digit hex code for your text color without the #:",
-    name: "colorHex",
-    validate: function (answer) {
-      const numberValue = Number(answer);
-      if (answer.length > 6 || isNaN(numberValue)) {
-        return console.log(" Invalid entry. Please enter a 6 digit hex code.");
-      }
-      return true;
-    },
-    when: (answers) => answers.colorChoice.includes("hex"),
-  },
-  {
-    type: "list",
-    message: "What is your preferred method of communication?",
-    name: "license",
-    choices: [
-      { name: "circle", value: "circle" },
-      { name: "triangle", value: "triangle" },
-      { name: "square", value: "square" },
-    ],
-  },
-]).then((answers) => {
-  console.log(answers);
+const questions = require("./lib/questions");
+
+prompt(questions).then((answers) => {
+  fs.writeFileSync("logo.svg", generateSVG(answers));
 });
