d94cb559af0877b17717193d9416081d4fc5b387
Zgibbs58 <zacharywgibbs@gmail.com>
2023-06-20 23:45:07 -0500

    updated shapes.js to hold all rendering of logo in the shapes renderLogo()  method. Making the generateSVG.js file obsolete.

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
-const generateSVG = require("./lib/generateSVG");
-const shape = require("./lib/shapes");
+// const generateSVG = require("./lib/generateSVG");
+const shapes = require("./lib/shapes");
 
 const prompt = inquirer.createPromptModule();
 
 const questions = require("./lib/questions");
 
 prompt(questions).then((answers) => {
-  fs.writeFileSync("./examples/logo.svg", generateSVG(answers));
+  // defining shape as the shape object from shapes.js
+  const shape = shapes[answers.shape];
+  fs.writeFileSync(
+    "./examples/logo.svg",
+    shape.renderLogo(
+      shape.code,
+      answers.colorChoice.includes("hex") ? "#" + answers.shapeColor : answers.shapeColor,
+      answers.text,
+      answers.colorChoice.includes("hex") ? "#" + answers.textColor : answers.textColor
+    )
+  );
 });
