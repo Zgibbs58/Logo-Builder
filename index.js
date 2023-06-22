755b259087bf3ae0bd77f835a3df9d145f5a2193
Zgibbs58 <zacharywgibbs@gmail.com>
2023-06-22 12:01:51 -0500

    Made the project more modular by putting the renderLogo function in it's own file and adding a renderShape method in the shape classes.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // and can return promises that can be chained using .then() and .catch() like in exercise 24-Stu_Promises-Catch
 const fs = require("fs/promises");
 const inquirer = require("inquirer");
-// const generateSVG = require("./lib/generateSVG");
+const generateLogo = require("./lib/generateLogo");
 const { shape, circle, triangle, square } = require("./lib/shapes");
 
 const prompt = inquirer.createPromptModule();
@@ -11,18 +11,18 @@ const questions = require("./lib/questions");
 
 prompt(questions)
   .then((answers) => {
-    let shape;
+    let shapeChoice;
     if (answers.shape === "circle") {
-      // This is just answers.shapeColor
+      // This is just new shapeChoice(answers.shapeColor)
       // added a ternary operator to check if the user inputted a hex color code to add the # to the beginning of the string
-      shape = new circle(answers.colorChoice.includes("hex") ? "#" + answers.shapeColor : answers.shapeColor);
+      shapeChoice = new circle(answers.colorChoice.includes("hex") ? "#" + answers.shapeColor : answers.shapeColor);
     } else if (answers.shape === "triangle") {
-      shape = new triangle(answers.colorChoice.includes("hex") ? "#" + answers.shapeColor : answers.shapeColor);
+      shapeChoice = new triangle(answers.colorChoice.includes("hex") ? "#" + answers.shapeColor : answers.shapeColor);
     } else if (answers.shape === "square") {
-      shape = new square(answers.colorChoice.includes("hex") ? "#" + answers.shapeColor : answers.shapeColor);
+      shapeChoice = new square(answers.colorChoice.includes("hex") ? "#" + answers.shapeColor : answers.shapeColor);
     }
     // created a variable for the renderLogo method from shapes.js for readability
-    const generatedSVG = shape.renderLogo(answers.text, answers.colorChoice.includes("hex") ? "#" + answers.textColor : answers.textColor);
+    const generatedSVG = generateLogo(answers.text, answers.colorChoice.includes("hex") ? "#" + answers.textColor : answers.textColor, shapeChoice);
     return fs.writeFile("./examples/logo.svg", generatedSVG);
   })
   .then(() => console.log("Generated logo.svg"))
