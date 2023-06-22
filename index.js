1fad3e51e9111c6e2ea9542778e32158c2ef088c
Zgibbs58 <zacharywgibbs@gmail.com>
2023-06-21 21:32:29 -0500

    Added require fs/promises to utilize functionality of the promise chain on writeFile so that after the file is successfully written there is a message in the console.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const fs = require("fs");
+// used fs/promises instead of fs because it is a promise based version of the fs module
+// and can return promises that can be chained using .then() and .catch() like in exercise 24-Stu_Promises-Catch
+const fs = require("fs/promises");
 const inquirer = require("inquirer");
 // const generateSVG = require("./lib/generateSVG");
 const shapes = require("./lib/shapes");
@@ -7,16 +9,21 @@ const prompt = inquirer.createPromptModule();
 
 const questions = require("./lib/questions");
 
-prompt(questions).then((answers) => {
-  // defining shape as the shape object from shapes.js
-  const shape = shapes[answers.shape];
-  fs.writeFileSync(
-    "./examples/logo.svg",
-    shape.renderLogo(
+prompt(questions)
+  .then((answers) => {
+    // defining shape as the shape object from shapes.js
+    const shape = shapes[answers.shape];
+    // created a variable for the renderLogo method from shapes.js for readability
+    const generatedSVG = shape.renderLogo(
       shape.code,
       answers.colorChoice.includes("hex") ? "#" + answers.shapeColor : answers.shapeColor,
       answers.text,
       answers.colorChoice.includes("hex") ? "#" + answers.textColor : answers.textColor
-    )
-  );
-});
+    );
+    return fs.writeFile("./examples/logo.svg", generatedSVG);
+  })
+  .then(() => console.log("Generated logo.svg"))
+  .catch((err) => {
+    console.log(err);
+    console.log("Unable to write the file logo.svg");
+  });
